e33f214f3da6b293abf65cf18d8c307dd7a40d64
VIRENDRA-18 <virusingh0125@gmail.com>
2025-12-30 23:42:30 +0530

    Refactor game logic and enhance styling for Rock Paper Scissors game

--- Rock Paper Scissors Game/app.js
+++ Rock Paper Scissors Game/app.js
@@ -5,27 +5,26 @@ const gameContainer = document.querySelector(".container"),
   result = document.querySelector(".result"),
   optionImages = document.querySelectorAll(".option_image");
 
-// Loop through each option image element
 optionImages.forEach((image, index) => {
   image.addEventListener("click", (e) => {
     image.classList.add("active");
 
-    // Loop through each option image again
+    userResult.src = cpuResult.src = "images/rock.png";
+    result.textContent = "Wait...";
+
     optionImages.forEach((image2, index2) => {
-      // If the current index doesn't match the clicked index
-      // Remove the "active" class from the other option images
+      
       index !== index2 && image2.classList.remove("active");
     });
 
     gameContainer.classList.add("start");
 
-    //set a timeout to delyy the result display
-    let timeout = setTimeout(() => {
+    
+    let time = setTimeout(() => {
         gameContainer.classList.remove("start");
-            // Get the source of the clicked option image
+           
 let imageSrc = e.target.querySelector("img").src;
 
-// Set the user image to the clicked option image
 userResult.src = imageSrc;
 
 // Generate a random number between 0 and 2
@@ -62,8 +61,10 @@ let outcomes = {
 
 // Look up the outcome value based on user and CPU options
 let outcomeValue = outcomes[userValue + cpuValue];
-    },2000);
+    
 
-  
+  // display the result 
+    result.textContent = userValue ===cpuValue ? "Match Draw" : `${outcomeValue} Won!!`;
+    },2500);
   });
 });
