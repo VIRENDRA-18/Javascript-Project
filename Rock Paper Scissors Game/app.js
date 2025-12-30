593a790aa8e66127c55575305d8d7afe3c372db1
VIRENDRA-18 <virusingh0125@gmail.com>
2025-12-30 22:12:28 +0530

    Implement option image selection and styling for Rock Paper Scissors game

---
+++ Rock Paper Scissors Game/app.js
@@ -0,0 +1,20 @@
+// Get DOM elements
+const gameContainer = document.querySelector(".container"),
+  userResult = document.querySelector(".user_result img"),
+  cpuResult = document.querySelector(".cpu_result img"),
+  result = document.querySelector(".result"),
+  optionImages = document.querySelectorAll(".option_image");
+
+// Loop through each option image element
+optionImages.forEach((image, index) => {
+  image.addEventListener("click", () => {
+    image.classList.add("active");
+
+    // Loop through each option image again
+    optionImages.forEach((image2, index2) => {
+      // If the current index doesn't match the clicked index
+      // Remove the "active" class from the other option images
+      index !== index2 && image2.classList.remove("active");
+    });
+  });
+});
